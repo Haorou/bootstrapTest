2fc9d140a9e93e98dc12d15238bf55a36cac8b7c
haorou <haorou@live.fr>
2018-10-01 17:57:12 +0200

    bootstrap dude

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Bootstrap</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
