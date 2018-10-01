59685d1bff33061a928abd165aa927b765f2ec32
haorou <julien.brulle@gmail.com>
2018-10-01 22:09:36 +0200

    bootstrap begin

--- index.php
+++ index.php
@@ -1,14 +1,43 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
     <head>
-        <meta charset="utf-8"/>
-        <title>Bootstrap</title>
-        <link href="bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <title>Bootstrap template</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="bootstrap/css/tuto.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
     </head>
 
     <body>
+        <h1 class>Hello, world!</h1>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 ">6 colonnes
+                    <div class="row">
+                        <div class="col-lg-4"> 4 colonnes</div>
+                        <div class="col-lg-offset-4 col-lg-4">4 colonnes</div>
+                    </div>
+                </div>
+                <div class="col-lg-6 ">6 colonnes
 
-        <script src="bootstrap-3.3.7/js/jquery.js"></script>
-        <script src="bootstrap-3.3.7/css/bootstrap.min.css"></script>
+                    <div class="row">
+                        <div class="col-lg-4"> 4 colonnes</div>
+                        <div class="col-lg-8"> 8 colonnes</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
